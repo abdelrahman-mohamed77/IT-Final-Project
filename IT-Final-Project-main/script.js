ece78a8ba7d59c3371247344a75b6d390a5692ec
abdelrhman-mohamed0-0 <abdo.osman900000@gmail.com>
2026-04-24 15:43:47 +0200

    Add files via upload

---
+++ IT-Final-Project-main/script.js
@@ -0,0 +1,50 @@
+let darkmode = localStorage.getItem("dark-mod");
+const themeSwitch = document.getElementById("theme-switch");
+
+const enableDarkmode = () => {
+  document.body.classList.add("dark-mod");
+  localStorage.setItem("dark-mod", "active");
+};
+const disableDarkmode = () => {
+  document.body.classList.remove("dark-mod");
+  localStorage.setItem("dark-mod", "inactive");
+};
+if (darkmode === "active") {
+  enableDarkmode();
+}
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("dark-mod");
+  if (darkmode !== "active") {
+    enableDarkmode();
+  } else {
+    disableDarkmode();
+  }
+});
+
+
+
+
+
+/* Product Filtering*/
+const filterButtons = document.querySelectorAll('.filter-btn');
+const productCards = document.querySelectorAll('.product-card');
+const producttext = document.querySelectorAll('.product-text');
+filterButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    
+    const filterValue = button.getAttribute('data-filter');
+
+    productCards.forEach(function(card) {
+      const cardCategory = card.getAttribute('data-category') || "";
+      const cardSize = card.getAttribute('data-size') || "";
+      const cardPrice = card.getAttribute('data-price') || "";
+      if (cardCategory === filterValue || cardSize === filterValue || cardPrice === filterValue) {
+        card.style.display = 'flex';
+        card.style.opacity = '0';
+        setTimeout(() => { card.style.opacity = '1'; }, 150);
+      } else {
+        card.style.display = 'none'; 
+      }
+    });
+  });
+});
