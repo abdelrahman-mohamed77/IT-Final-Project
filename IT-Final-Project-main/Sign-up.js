67a256b7add2edf5a1cd9864f8580ed07eefc647
abdallahayman1928374652@gmail.com <abdallahayman1928374652@gmail.com>
2026-05-03 11:30:28 +0300

    Update sign-up logic

--- IT-Final-Project-main/Sign-up.js
+++ IT-Final-Project-main/Sign-up.js
@@ -117,3 +117,29 @@ signInForm.addEventListener("submit", (e) => {
     errorMessageElement.textContent = "Wrong username or password.";
   }
 });
+////////////////////////////
+/*DARK MODE*/
+///////////////////////////
+
+let darkmode = localStorage.getItem("dark-mod");
+const themeSwitch = document.getElementById("theme-switch");
+
+const enableDarkmode = () => {
+  document.body.classList.add("dark-mod");
+  localStorage.setItem("dark-mod", "active");
+};
+const disableDarkmode = () => {
+  document.body.classList.remove("dark-mod");
+  localStorage.setItem("dark-mod", "inactive");
+};
+if (darkmode === "active") {
+  enableDarkmode();
+}
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("dark-mod");
+  if (darkmode !== "active") {
+    enableDarkmode();
+  } else {
+    disableDarkmode();
+  }
+});
